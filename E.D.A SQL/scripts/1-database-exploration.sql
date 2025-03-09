6465fafae62f861dc8a2daed04e3d7776276f5a5
Itin <thenewera07@gmail.com>
2025-03-09 16:12:59 -0300

    projeto novo sql

---
+++ E.D.A SQL/scripts/1-database-exploration.sql
@@ -0,0 +1,20 @@
+/*
+============================================
+Exploração do banco de dados
+============================================
+Finalidade:
+	- Explorar a estrutura do banco de dados, incluindo a lista de tabelas e seus schemas;
+	- Inspecionar as colunas e metadados de tabelas específicas;
+
+Tabela usadas:
+	- INFORMATION_SCHEMA.TABLES
+	- INFORMATION_SCHEMA.COLUMNS
+
+
+SELECT DISTINCT
+category
+FROM [gold.dim_products]
+
+SELECT DISTINCT
+sales_amount
+FROM [gold.fact_sales]
